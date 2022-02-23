7dda44c3a0e1f67fd4fcbaa3268c4a4187bd49a5
Clemens Winter <clemenswinter1@gmail.com>
2022-02-22 20:20:56 -0800

    Reformat

--- hyperstate/__init__.py
+++ hyperstate/__init__.py
@@ -4,4 +4,15 @@ from .schema.versioned import Versioned
 from .schema.schema_checker import schema_evolution_cli
 
 
-__all__ = ["HyperState", "load", "loads", "dump", "dumps", "blob", "Serializable", "Lazy", "Versioned", "schema_evolution_cli"]
+__all__ = [
+    "HyperState",
+    "load",
+    "loads",
+    "dump",
+    "dumps",
+    "blob",
+    "Serializable",
+    "Lazy",
+    "Versioned",
+    "schema_evolution_cli",
+]
