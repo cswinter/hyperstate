55babf6ab91785b8ff0d31145537a1626fa83576
Clemens Winter <clemenswinter1@gmail.com>
2022-04-16 10:44:23 -0700

    Fix non-atomic checkpoint creation

--- hyperstate/hyperstate.py
+++ hyperstate/hyperstate.py
@@ -1,8 +1,9 @@
 import os
+import errno
+import os
 import shutil
 from abc import ABC, abstractmethod
 from pathlib import Path
-import sys
 import tempfile
 from typing import (
     Generic,
@@ -16,10 +17,9 @@ from typing import (
     Union,
 )
 from dataclasses import MISSING, dataclass
-import hyperstate
+import uuid
 from hyperstate.schema.types import Struct, materialize_type
 import hyperstate.schema.types as t
-import click
 
 from hyperstate.schema.versioned import (
     Versioned,
@@ -126,7 +126,7 @@ class HyperState(ABC, Generic[C, S]):
             p.mkdir()
             _typed_dump(self.config, p / "config.ron", self.schedules)
             _typed_dump(self.state, p / "state.ron")
-            shutil.move(str(p), target_dir)
+            atomic_move(str(p), target_dir)
 
     def step(self) -> None:
         _apply_schedules(self.state, self.config, self.schedules)
@@ -444,3 +444,40 @@ class ElideDefaults(Serializer):
                 and attrs[name] == field.default_factory()
             ):
                 del attrs[name]
+
+
+def atomic_move(src: str, dst: str) -> None:
+    """Rename a file from ``src`` to ``dst``.
+
+    *   Moves must be atomic.  ``shutil.move()`` is not atomic.
+        Note that multiple threads may try to write to the cache at once,
+        so atomicity is required to ensure the serving on one thread doesn't
+        pick up a partially saved image from another thread.
+
+    *   Moves must work across filesystems.  Often temp directories and the
+        cache directories live on different filesystems.  ``os.rename()`` can
+        throw errors if run across filesystems.
+
+    So we try ``os.rename()``, but if we detect a cross-filesystem copy, we
+    switch to ``shutil.move()`` with some wrappers to make it atomic.
+    """
+    try:
+        os.rename(src, dst)
+    except OSError as err:
+
+        if err.errno == errno.EXDEV:
+            # Generate a unique ID, and copy `<src>` to the target directory
+            # with a temporary name `<dst>.<ID>.tmp`.  Because we're copying
+            # across a filesystem boundary, this initial copy may not be
+            # atomic.  We intersperse a random UUID so if different processes
+            # are copying into `<dst>`, they don't overlap in their tmp copies.
+            copy_id = uuid.uuid4()
+            tmp_dst = f"{dst}.{copy_id}.tmp" % (dst, copy_id)
+            shutil.copyfile(src, tmp_dst)
+
+            # Then do an atomic rename onto the new name, and clean up the
+            # source image.
+            os.rename(tmp_dst, dst)
+            os.unlink(src)
+        else:
+            raise
